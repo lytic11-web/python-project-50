65f2ebc901145d66a3cd92cf808b5ae075307278
lytic11 <lytic11@gmail.com>
2026-01-14 17:25:20 +0300

    step 2: add --format option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,12 +8,18 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    
+
+
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help='set format of output'
+    )
     args = parser.parse_args()
     
     # Для первого шага просто выводим имена файлов
     print(f"Comparing {args.first_file} and {args.second_file}")
-
+    print(f"Output format: {args.format}")
 
 if __name__ == '__main__':
     main()
