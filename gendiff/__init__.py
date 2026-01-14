625a740dfd6c147ffd6fa58dae019cd58b298d9a
lytic11 <lytic11@gmail.com>
2026-01-14 13:56:16 +0300

    feat: add basic CLI for gendiff utility

---
+++ gendiff/__init__.py
@@ -0,0 +1,4 @@
+from gendiff.scripts.gendiff import main
+
+__version__ = '0.1.0'
+__all__ = ['main']
---
+++ gendiff/core.py
@@ -0,0 +1,4 @@
+def generate_diff(file1_path, file2_path):
+    """Основная функция сравнения файлов."""
+    # Пока заглушка
+    return "Difference will be here"
