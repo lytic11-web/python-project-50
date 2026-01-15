851b133cd8d2a930efece24435615396e371047a
lytic11 <lytic11@gmail.com>
2026-01-15 19:58:02 +0300

    feat: implement reading and parsing JSON files

--- gendiff/core.py
+++ gendiff/core.py
@@ -1,4 +1,23 @@
-def generate_diff(file1_path, file2_path):
-    """Основная функция сравнения файлов."""
-    # Пока заглушка
-    return "Difference will be here"
+import json
+
+
+def read_file(file_path):
+    """
+    Читаем и парсим JSON фаил. Предполагаем, что
+    он "чёткий и првильный"
+    """
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+
+def generate_diff(file1_path, file2_path, format='stylish'):
+    """
+    Основная функция сравнения файлов.
+    """
+    # Читаем файлы
+    data1 = read_file(file1_path)
+    data2 = read_file(file2_path)
+    
+    # Пока просто выводим содержимое файлов
+    return f"File 1: {data1}\nFile 2: {data2}" 
